5c1740dae3ee06e5cb08cab3f6e491dd8e47f4ec
Aadrika-Shukla <aadrikashukla19@gmail.com>
2024-05-17 16:25:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from app.models import *
 
 
-
+'''
 def insert_topic(request):
     if request.method=='POST':
         tn=request.POST['tn']
@@ -49,4 +49,62 @@ def insert_access(request):
         AO.save()
         return HttpResponse('record for respective topic is successfully inserted to my database')
 
-    return render(request,'insert_access.html')
+    return render(request,'insert_access.html')'''
+
+
+
+
+###### inorder to display topic names in form of dropdown list so that  user can't enter new topics as previously we are using get method so if topic is not their if will throw error and using get_or_create to create that is it is not their now user can't enter new topic for child table as we created dropdow thus eradicating that errror  ####
+
+
+def insert_topic(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        TO=Topic.objects.get_or_create(topic_name=tn)[0]
+        TO.save()
+        return HttpResponse('Topic is successfully inserted to my database')
+    return render(request,'insert_topic.html')
+
+
+
+
+
+def insert_webpage(request):
+    QLTO=Topic.objects.all() # to get all the objects from the topic table so that it can be sended from be to fe in order to get display it
+    d={'QLTO':QLTO}
+
+    if request.method=='POST':
+        tn=request.POST['tn']
+        na=request.POST['na']
+        ur=request.POST['ur']
+        em=request.POST['em']
+        RTO=Topic.objects.get(topic_name=tn)#as we sended topic_name from  fe so we are using topic_name here for comparison
+        WO=WebPage.objects.get_or_create(topic_name=RTO,name=na,url=ur,email=em)[0]
+        WO.save()
+        return HttpResponse('webpage for respective topic is successfully inserted to my database')
+    
+        
+
+
+    return render(request,'insert_webpage.html',d)#sending the topic objects from be to fe
+
+
+
+
+
+
+def insert_access(request):
+    QLWO=WebPage.objects.all() # to get all the objects from the webpage table so that it can be sended from be to fe in order to get display it
+    d={'QLWO':QLWO}
+    if request.method=='POST':
+        na=request.POST['na']
+        da=request.POST['da']
+        at=request.POST['at']
+        RWO=WebPage.objects.get(id=na)#as we sended id because it is primary key column from  fe so we are using id here for comparison
+        AO=AccessRecord.objects.get_or_create(name=RWO,date=da,author=at)[0]
+        AO.save()
+        return HttpResponse('record for respective topic is successfully inserted to my database')
+
+    return render(request,'insert_access.html',d)#sending the topic objects from be to fe
+
+
